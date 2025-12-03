01f0661dd5daaec01c667efda821fc5647ac31e7
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-12-03 16:14:31 +0100

    feat: implement solution for day03 part2

--- cmd/day03/main.go
+++ cmd/day03/main.go
@@ -4,6 +4,8 @@ import (
 	"aoc2025/internal/utils"
 	"fmt"
 	"strconv"
+	"sync"
+	"sync/atomic"
 )
 
 var (
@@ -39,5 +41,36 @@ func partOne() {
 }
 
 func partTwo() {
+	var sum atomic.Int64
+	var wg sync.WaitGroup
 
+	for _, line := range dayInput {
+		wg.Go(func() {
+			highestJoltage, _ := strconv.ParseInt(findHighestJoltage(line, 12), 10, 64)
+			sum.Add(highestJoltage)
+		})
+	}
+	wg.Wait()
+	fmt.Printf("PartTwo: %d\n", sum.Load())
+}
+
+func findHighestJoltage(nums string, digits int8) string {
+	if digits == 0 {
+		return ""
+	}
+
+	searchableSubstr := nums[:len(nums)-int(digits)+1]
+
+	highestJoltage := '0'
+	highestJoltageIndex := 0
+	for i, char := range searchableSubstr {
+		if char > highestJoltage {
+			highestJoltage = char
+			highestJoltageIndex = i
+		}
+	}
+
+	remainingSubstr := nums[highestJoltageIndex+1:]
+
+	return string(highestJoltage) + findHighestJoltage(remainingSubstr, digits-1)
 }
